8e18d8999bd6c069fd944ba30a434d187bc842f9
Adrian Messias <adrianmessias62@gmail.com>
2025-07-28 16:30:23 -0300

    feat: created get-user-profile use case and check-in

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,11 @@
 import { prisma } from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
   async findByEmail(email: string) {
     const User = await prisma.user.findUnique({
       where: {
@@ -18,3 +21,4 @@ export class PrismaUsersRepository implements UsersRepository {
     return user;
   }
 }
+
